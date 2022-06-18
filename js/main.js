6609843a2cbd37c5756685a26f26823417098f70
luisjairm <luisjairm.dev@gmail.com>
2022-06-18 12:54:28 -0500

    add: Inteccion de los botones con js

--- js/main.js
+++ js/main.js
@@ -1,50 +1,27 @@
-const contador = document.querySelector('main.container .contador');
-const contadorValue = document.querySelector('main.container .contador p.contador-value')
+// Variables
+const buttons = document.querySelector('.btn-group')
+const cardValue = document.querySelector('#card-value')
 
-let cont = 1
+
+// Eventos
 escuchaEventos();
 function escuchaEventos(){
-    contador.addEventListener('click', accionesBtn)
+    buttons.addEventListener('click', botonesAccion)
 }
 
-
-function accionesBtn(e){
-    
-    if(e.target.classList.contains('aumentar')){
-        let value = parseInt(contadorValue.textContent)
-        value++
-        contadorValue.textContent = value
-    }
-    if(e.target.classList.contains('resetear')){
-        
-        contadorValue.textContent = 0
-    }
-    if(e.target.classList.contains('disminuir')){
-        let value = parseInt(contadorValue.textContent)
+function botonesAccion(e){
+    if(e.target.classList.contains('dism')){
+        let value = parseInt(cardValue.textContent)
         value--
-        contadorValue.textContent = value
+        cardValue.textContent = value
     }
-
-}
-
-function generaContador(){
-    const container = document.querySelector('main.container')
-
-    const genContador = document.createElement('div')
-    genContador.classList.add('contador')
-
-    const estructura = `
-        <button class=" btn cerrar"> X </button>
-        <p class="contador-value data-id${cont}">0</p>
-        <div class="container-buttons"> 
-            <button class="btn aumentar"> +1 </button>
-            <button class="btn resetear"> 0 </button>
-            <button class="btn disminuir"> -1 </button>
-        </div>
-    `
-    genContador.innerHTML = estructura
-    container.appendChild(genContador)
-    cont++
-    return container
-    
+    if(e.target.classList.contains('reset')){
+        
+        cardValue.textContent = 0
+    }
+    if(e.target.classList.contains('aument')){
+        let value = parseInt(cardValue.textContent)
+        value++
+        cardValue.textContent = value
+    }
 }
